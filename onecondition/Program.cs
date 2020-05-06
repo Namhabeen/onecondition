059fc0f5d283e30b00b3186964d273cf56ca3bef
South_under_been <s2018s27@e-mirim.hs.kr>
2020-05-06 23:43:21 +0900

    초기 커밋.

---
+++ onecondition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onecondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("X 입력: ");
+            x = int.Parse(Console.ReadLine());
+
+            if (x > 10 && x < 20)
+            {
+                Console.WriteLine("조건에 맞습니다!");
+            }
+        }
+    }
+}
